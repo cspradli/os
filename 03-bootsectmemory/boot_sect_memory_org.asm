006a327b9b4bb98eaa23f69e5e6c352c60722bf3
Caleb Spradlin <cspradli@unca.edu>
2019-03-29 14:42:06 -0400

    added org, solves mem probelm

---
+++ 03-bootsectmemory/boot_sect_memory_org.asm
@@ -0,0 +1,47 @@
+[org 0x7c00]
+mov ah, 0x0e
+
+;this attempt failed because it tried to print the memory address
+;still fails regardless of the org, still not point to the data
+mov al, "1"
+int 0x10
+mov al, the_secret
+int 0x10
+
+;UPDATE: Having solved the correct problem with org, this now works
+;It tried to print the mem address of 'the_secret' which is the correct approach
+; BIOS places our boot_sect mem at address 0x7c00 so we need to add that padding before
+mov al, "2"
+int 0x10
+mov al, [the_secret]
+int 0x10
+
+;UPDATE: As expected we are adding 0x7c00 twice so it will not work
+;ATTEMPT 3 DEAR GOD
+; add bios offset and then deref the contents of that pointer
+; need to use bx because mova al, [ax] is no bueno
+; a register cant be used as a source and dest at the same time
+mov al, "3"
+int 0x10
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+;UPDATE: this still works
+;Lets try shortcuts
+;dont want to be recounting label offsets every time we change code
+mov al, "4"
+int 0x10
+mov al, [0x7c2d]
+int 0x10
+
+jmp $ ;love infinite loops
+
+the_secret:
+    ;ASCII code X is stored just before the zero-padding
+    db "X"
+
+;zero padding and magic number for BIOS
+times 510-($-$$) db 0
+dw 0xaa55
